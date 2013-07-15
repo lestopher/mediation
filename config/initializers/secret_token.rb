d1669a52bc83a8166c289aa7df50fe5ffb403f96
Christopher Nguyen <christoph.nguyen@gmail.com>
2013-07-14 18:35:40 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mediation::Application.config.secret_token = '051e18e9731a65a21d9e3392d0143d2c59c95a8c22869072d5339888c68cba8b50f0759e44e55d0332c62b893ed1039d3d26f087b7edbd8df34954a748bb2d9d'
